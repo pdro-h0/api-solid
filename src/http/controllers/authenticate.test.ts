6929399eba4100df4a5ff4e2e203bed214ae732b
pdro-h0 <pedro.pho792@gmail.com>
2024-04-23 21:29:39 -0300

    feat: create authenticate, profile and register test (e2e)

---
+++ src/http/controllers/authenticate.test.ts
@@ -0,0 +1,29 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "../../app";
+import request from "supertest";
+
+describe("PROFILE (e2e)", () => {
+  beforeAll(async () => {
+    app.ready();
+  });
+  afterAll(async () => {
+    app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "test-01",
+      email: "test01@email.com",
+      password: "123456",
+    });
+    const response = await request(app.server).post("/sessions").send({
+      email: "test01@email.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
---
+++ src/http/controllers/register.test.ts
@@ -0,0 +1,27 @@
+import "dotenv/config"
+
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "../../app";
+import request from "supertest";
+
+describe("REGISTER (e2e)", () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    console.log(process.env.DATABASE_URL)
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "test-01",
+      email: "test01@email.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
